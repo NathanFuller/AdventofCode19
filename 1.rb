90276b66842919a9f10f309a54e559a201471023
Nate Fuller <nthn.fllr@gmail.com>
2019-12-04 20:23:36 -0700

    Part 1 done. Learned about reading from files, defining functions, and to_i.

--- 1.rb
+++ 1.rb
@@ -1,2 +1,11 @@
-input = File.in("1input.txt", 'r')
+def getFuel(mass)
+  return mass / 3 - 2
+end
 
+total = 0
+
+File.open("1input.txt", 'r').readlines.each do |mass|
+  total += getFuel(mass.to_i)
+end
+
+puts total
