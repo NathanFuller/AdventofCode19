c33a6162f88b384a3350f6275855300eac501369
Nate Fuller <nthn.fllr@gmail.com>
2019-12-04 20:39:39 -0700

    Learned about how Ruby does if/else, and tossed in a little recursion as well.

--- 1.rb
+++ 1.rb
@@ -2,10 +2,26 @@ def getFuel(mass)
   return mass / 3 - 2
 end
 
+def getFuelforFuel(mass)
+  mff = mass / 3 - 2
+  if mff <= 0
+    return 0
+  else
+    return mff + getFuelforFuel(mff)
+  end
+end
+
+#First dragon: Mass of module. Knows how much fuel for it.
+#Next dragon: Mass of that fuel. Knows how much fuel to carry *it*. 
+#.
+#.
+#Last dragon: Mass of that fuel. Knows we don't need more for it.
+
+
 total = 0
 
 File.open("1input.txt", 'r').readlines.each do |mass|
-  total += getFuel(mass.to_i)
+  total += getFuelforFuel(mass.to_i)
 end
 
 puts total
